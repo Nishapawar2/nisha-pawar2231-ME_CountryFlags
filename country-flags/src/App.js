c543ccd2f8f635c806df01b2e1b889c341e94499
unknown <nisha.pawar2231@gmail.com>
2024-05-09 18:13:27 +0530

    Second Commit-code

--- country-flags/src/App.js
+++ country-flags/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState, useEffect } from "react";
+import axios from "axios";
+export default function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => setCountries(response.data))
+      .catch((error) => console.log(error));
+  }, [countries]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="MainContainer">
+        {countries.map((country) => {
+          return (
+            <div key={country.cca3} className="CardContainer">
+              <img
+                className="FlagImage"
+                src={country.flags.png}
+                alt={`flag of ${country.name.common}`}
+              />
+              <h2>{country.name.common}</h2>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
